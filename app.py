0f8f96ea931fa196a75229c14d6d86df64298b44
Jonathan Stern <jstern718@gmail.com>
2024-12-15 17:42:46 -0500

    starting to look like a page

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 env_config = os.getenv("PROD_APP_SETTINGS", "config.DevelopmentConfig")
 app.config.from_object(env_config)
 
-@app.route("/")
-def index():
-  return "Hello World!"
+from lists.thinList import thinList
+from text.thinText import thinText
+print ("thinText", thinText)
 
+# @app.route("/")
+# def index():
+#   return "Hello World!"
+
+
+# @app.route("/sidebar")
+# def sidebar():
+#     return render_template('sidebar.html', isNavOpen=False, pillowList=thin)
+
+
+@app.route("/thin")
+def thin():
+    return render_template('page_template.html',
+                            isNavOpen=False,
+                            pillowList = thinList,
+                            pageText = thinText,
+                            pageTitle="The Best Thin Pillows")
