fc0f6ca7b606b654a988b5e3e69febc69fbec380
Jonathan Stern <jstern718@gmail.com>
2024-12-29 19:19:54 -0500

    working on blog post formatting and page

---
+++ lists/dealsList.py
@@ -0,0 +1,17 @@
+dealsList = [
+    {"date": "12/29/24",
+    "intro": "A good deal on a thin pillow",
+    "title": "DOWNLITE Flat & Soft Down Pillow",
+    "normalprice": "$20.00",
+    "dealprice": "$15.00",
+    "see": "/thin",
+    "link": "https://amzn.to/4elSMj5"},
+    {"date": "12/29/24",
+    "intro": "A good deal on a thin pillow",
+    "title": "DOWNLITE Flat & Soft Down Pillow",
+    "normalprice": "$20.00",
+    "dealprice": "$15.00",
+    "see": "/thin",
+    "link": "https://amzn.to/4elSMj5"},
+]
+
