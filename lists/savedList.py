fb9927c8e670fbcd07e8829011d7c61552f3d442
Jonathan Stern <jstern718@gmail.com>
2025-01-02 16:36:10 -0500

    correcting memory foam links

---
+++ lists/savedList.py
@@ -0,0 +1,10 @@
+savedMemoryFoamList = [
+    {"intro": "The Best Memory Foam Pillow For People Who Have More To Spend",
+      "title": "Casper Sleep Foam Pillow, Standard",
+      "description": "One of the problems in the pillow market is that it can be really hard to distinguish between materials, especially when those materials have similar names. Sometimes, the namebrand pillow and the knockoff are made of similar materials. Sometimes they aren't. That's why it can pay to try leveling up. This is a foam pillow that really does feel like it is made with a better quality foam. The difference isn't enormous, but it is there.",
+      "link": "https://amzn.to/3YJqxow"},
+    {"intro": "Alternative Best Memory Foam Pillow For People Who Need A Thin Pillow",
+      "title": "USlixury Adjustable Side Sleeper Pillow",
+      "description": "If your looking for a reasonably priced cooling pillow, but the firmness of buckwheat hulls doesn't work for you and the propriety foam blends don't really help, this option with a gel layer might be the best option for you. The gel layer here won't keep you cool for ever, but it lasts much longer than cooling pillow covers.",
+      "link": "https://amzn.to/40NtpU8"}
+]
