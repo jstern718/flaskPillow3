a11c5fdd11428da4e95db72b25026499b9ae7fcf
Jonathan Stern <jstern718@gmail.com>
2025-01-05 14:18:31 -0500

    fixing font sizing bug in mobile

--- lists/dealsList.py
+++ lists/dealsList.py
@@ -1,10 +1,10 @@
 dealsList = [
     {"date": "1.3.25",
-    "intro": "A good deal on a memory foam pillow that is adjustable in height:",
+    "intro": "A Good Deal on a Memory Foam Pillow that is Adjustable in Height:",
     "title": "USlixury Adjustable Side Sleeper Pillow",
     "normalprice": "$40.99",
     "dealprice": "$36.89",
-    "discussion": "Despite the less than admirable spelling of its name, this pillow is a good option for people who need a thin pillow. It is adjustable in height, so you can make it as thin as you need. It is also a great option for side sleepers because of the presence of a cutout that allows your shoulders to align with your head while you sleep on your side.",
+    "discussion": "Despite the less than admirable spelling of its name, this pillow is a good option for people who need a thin pillow. <br><br> It is adjustable in height, so you can make it as thin as you need. It is also a great option for side sleepers because of the presence of a cutout that allows your shoulders to align with your head while you sleep on your side.",
     "see": "/memory",
     "link": "https://amzn.to/40NtpU8"},
 ]
