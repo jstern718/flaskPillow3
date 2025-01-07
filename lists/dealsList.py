652c14676ea675f53b1db19e77cbce0e0eef30da
Jonathan Stern <jstern718@gmail.com>
2025-01-07 15:02:08 -0500

    adding to deal list

--- lists/dealsList.py
+++ lists/dealsList.py
@@ -1,4 +1,11 @@
 dealsList = [
+    { "date": "1.7.25",
+      "intro": "A Good Deal On an Affordable (and Fluffy) Set of Down Alternative Pillows",
+      "title": "Bedsure Down Alternative Pillows (set of 2)",
+      "normalprice": "$25.00",
+      "dealprice": "$19.79",
+      "description": "The Bedsure Pillows (queen) (which come in sets of 2) are down alternative pillows with a polyester fill that are approximately 9 1/2 inches thick (so definitely on the fluffy side).  They are incredibly popular, having an average rating of 4.3 stars with over 14,000 reviews.",
+      "link": "https://amzn.to/482LPC7"},
     {"date": "1.3.25",
     "intro": "A Good Deal on a Memory Foam Pillow that is Adjustable in Height:",
     "title": "USlixury Adjustable Side Sleeper Pillow",
